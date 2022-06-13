463b11f6e64fb1283b33df3cd10af264c1fcd670
inputTitleHere <seanbryan97@naver.com>
2022-06-13 16:55:34 +0900

    added method printFancyStuff()

--- src/com/kh/git/HelloGit.java
+++ src/com/kh/git/HelloGit.java
@@ -4,4 +4,8 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello GIT WORLD !");
 	}
+	
+	public void printFancyStuff() {
+		System.out.println("깃! 허! 브!");
+	}
 }
