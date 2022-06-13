5c4d2636ea07d7fbd5e74f45bd465532e66ace5e
inputTitleHere <seanbryan97@naver.com>
2022-06-13 17:38:39 +0900

    Dog 기능 완성

--- src/com/kh/git/HelloGit.java
+++ src/com/kh/git/HelloGit.java
@@ -1,10 +1,16 @@
 package com.kh.git;
 
+import com.kh.git.animal.Dog;
+
 public class HelloGit {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello GIT WORLD !");
 		new HelloGit().printFancyStuff();
+		
+		// Dog 기능 추가.
+		Dog dog = new Dog();
+		dog.bark();
 	}
 	
 	public void printFancyStuff() {
