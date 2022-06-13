81479aebeedd0a4f95b4d0a8e81984434d61f5cf
inputTitleHere <seanbryan97@naver.com>
2022-06-13 20:48:22 +0900

    Refitted whitespace

--- src/com/kh/git/animal/Snake.java
+++ src/com/kh/git/animal/Snake.java
@@ -1,11 +1,12 @@
 package com.kh.git.animal;
 
 public class Snake {
-    public void run(){
-      System.out.println("Snake Run~~~");
-      System.out.println("Another SNAKE");
-    }
-    public void eat(){
-        system.out.println("Snake Eats");
-    }
+	public void run() {
+		System.out.println("Snake Run~~~");
+		System.out.println("Another SNAKE");
+	}
+
+	public void eat() {
+		System.out.println("Snake Eats");
+	}
 }
