c301943f7d42ff9b2ea7e08ca849504a82232c41
inputTitleHere <seanbryan97@naver.com>
2022-06-14 15:58:15 +0900

    Owl name and species

--- src/com/kh/git/animal/Owl.java
+++ src/com/kh/git/animal/Owl.java
@@ -1,6 +1,10 @@
 package com.kh.git.animal;
 
 public class Owl {
+	
+	private String name = "Glaukopis";
+	private String species ="Athena Noctua";
+	
 	public void hoot() {
 		System.out.println("HOOT HOOT");
 	}
